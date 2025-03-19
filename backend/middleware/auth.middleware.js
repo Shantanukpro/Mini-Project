1fbc9cb1763d6ef80ef7314034bc4baaf7a67290
Yash Patkar <yashpatkar4249@gmail.com>
2025-03-19 22:05:46 +0530

    redis setup complete

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -2,17 +2,17 @@ import jwt from "jsonwebtoken";
 
 export const authUser = async (req, res, next) => {
 try {
- const taken = req.cookies.token || req.headers.authorization.split(' ')[1];
+ const token = req.cookies.token || req.headers.authorization.split(' ')[1];
 
  if (!token) {
     return res.status(401).send({ error: 'Unauthorized User' });
  }
 
- const decoded = jwt.verify(taken, process.env.JWT_SECRET);
+ const decoded = jwt.verify(token, process.env.JWT_SECRET);
  req.user = decoded;
  next();
 } catch (error) { 
-    console.error(error);   
+    console.log(error);   
     res.status(401).send({ error: 'Unauthorized User' });
 }
 
